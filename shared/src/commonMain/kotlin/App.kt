613dc083c5e624e1453d434cc0f8b9a29eb3dbd1
dima.avdeev <dima.avdeev@jetbrains.com>
2023-12-28 10:35:39 +0400

    reproduce

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,40 +1,117 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuBoxScope
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
-    MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello, World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Hello, ${getPlatformName()}"
-                showImage = !showImage
-            }) {
-                Text(greetingText)
-            }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    contentDescription = "Compose Multiplatform icon"
+    TextDropdownField(
+        state = rememberDropdownMenuState(setOf("A","B","C"), excludeSelectedItem = true),
+        readOnly = false,
+        itemText = { it }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun<T> TextDropdownField(
+    state: DropdownMenuState<T>,
+    readOnly: Boolean = true,
+    label: @Composable (() -> Unit)? = null,
+    itemText: (T) -> String
+) {
+    ExposedDropdownMenuBox(
+        expanded = state.expanded,
+        onExpandedChange = state::onExpandedChange,
+        content = state.boxContent(
+            readOnly = readOnly,
+            label = label,
+            itemText = itemText
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+private fun<T> DropdownMenuState<T>.boxContent(
+    readOnly: Boolean,
+    label: @Composable (() -> Unit)?,
+    itemText: (T) -> String
+): @Composable ExposedDropdownMenuBoxScope.() -> Unit = {
+    TextField(
+        value = textInput,
+        placeholder = { Text(itemText(selectedOption)) },
+        onValueChange = ::onValueChange,
+        modifier = Modifier.menuAnchor(),
+        readOnly = readOnly,
+        label = label,
+        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded) },
+        colors = ExposedDropdownMenuDefaults.textFieldColors(),
+    )
+
+    ExposedDropdownMenu(
+        expanded = expanded,
+        onDismissRequest = ::onDismissRequest,
+        content = {
+            val options = if(excludeSelectedItem) (options - selectedOption) else options
+
+            options.forEach { option ->
+                DropdownMenuItem(
+                    onClick = { onClick(option) },
+                    text = { Text(itemText(option)) },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
                 )
             }
         }
+    )
+}
+
+@Composable
+fun<T> rememberDropdownMenuState(
+    options: Set<T>,
+    init: T = options.first(),
+    excludeSelectedItem: Boolean = false
+) = remember { DropdownMenuState(options, excludeSelectedItem, init) }
+
+class DropdownMenuState<T>(
+    val options: Set<T>,
+    val excludeSelectedItem: Boolean,
+    init: T
+) {
+    var expanded by mutableStateOf(false)
+        private set
+    var selectedOption by mutableStateOf(init)
+        private set
+    var textInput by mutableStateOf(TextFieldValue())
+        private set
+
+    fun onClick(value: T) {
+        selectedOption = value
+        expanded = false
+    }
+
+    fun onValueChange(value: TextFieldValue) {
+        textInput = value
+        expanded = true
+    }
+
+    fun onExpandedChange(isExpanded: Boolean) {
+        expanded = isExpanded
+    }
+
+    fun onDismissRequest() {
+        expanded = false
     }
 }
 
